2dee2f9f2d9c24ac8112e102d8827d1b098bc26f
Peter <shencypeter@gmail.com>
2026-01-08 10:37:34 +0800

    delete files

--- CustomerFeedbackSystem/CoreProj2-master/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreProj2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
